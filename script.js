6702e412b5c8ca639536b13dace94d30fd79da75
Roee Angel <roee.angel93@gmail.com>
2020-09-08 11:51:59 +0300

    my weather is done TODO:search weather by city

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const apiKey = "61fed7decd915f9b031fb5a73d66db0c";
 const status = document.querySelector("#status");
 const mapLink = document.querySelector("#map-link");
 const my_weather = document.querySelector(".my-weather");
+const current_weather = document.querySelector(".current-weather");
+const sunrise = document.querySelector(".sunrise");
+const sunset = document.querySelector(".sunset");
 async function getMyWeather(lat, lon) {
   try {
     const data = await fetch(
@@ -11,6 +14,12 @@ async function getMyWeather(lat, lon) {
     );
     const myWeather = await data.json();
     console.log(myWeather);
+    const currentWeather = temperatureConverter(myWeather.main.temp);
+    current_weather.innerHTML += ": " + currentWeather;
+    sunrise.innerHTML += ": " + getUTCtimeStamp(myWeather.sys.sunrise);
+    sunset.innerHTML += ": " + getUTCtimeStamp(myWeather.sys.sunset);
+    getUTCtimeStamp();
+    console.log(currentWeather);
   } catch (e) {
     console.log(e);
   }
@@ -38,5 +47,19 @@ function geoFindMe() {
   } else {
     navigator.geolocation.getCurrentPosition(success, error);
   }
+}
+
+function temperatureConverter(valNum) {
+  valNum = parseFloat(valNum);
+  var num = Number(valNum - 273.15); // The Number() only visualizes the type and is not needed
+  var roundedString = num.toFixed(2);
+  return Number(roundedString);
+}
+function getUTCtimeStamp(valNum) {
+  var sec = valNum;
+  var date = new Date(sec * 1000);
+  var timestr = date.toLocaleTimeString();
+
+  return timestr;
 }
 window.onload = geoFindMe();
