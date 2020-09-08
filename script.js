b8dd707da672f3081c33760b4631a9a041a206c2
Roee Angel <roee.angel93@gmail.com>
2020-09-08 14:48:51 +0300

    search cities is working just add more style

--- script.js
+++ script.js
@@ -7,6 +7,13 @@ const my_weather = document.querySelector(".my-weather");
 const current_weather = document.querySelector(".current-weather");
 const sunrise = document.querySelector(".sunrise");
 const sunset = document.querySelector(".sunset");
+const city_input_holder = document.querySelector(".city-input-holder");
+const sumbit_btn = document.querySelector(".sumbit-btn");
+const search_city = document.querySelector(".search-city");
+const error_msg = document.querySelector(".error-msg");
+const search_weather = document.querySelector(".search-weather");
+const weather_icon = document.querySelector(".weather-icon");
+cities = {};
 async function getMyWeather(lat, lon) {
   try {
     const data = await fetch(
@@ -15,9 +22,10 @@ async function getMyWeather(lat, lon) {
     const myWeather = await data.json();
     console.log(myWeather);
     const currentWeather = temperatureConverter(myWeather.main.temp);
-    current_weather.innerHTML += ": " + currentWeather;
+    current_weather.innerHTML += ": " + currentWeather + "\xB0C.";
     sunrise.innerHTML += ": " + getUTCtimeStamp(myWeather.sys.sunrise);
     sunset.innerHTML += ": " + getUTCtimeStamp(myWeather.sys.sunset);
+    weather_icon.src = `http://openweathermap.org/img/wn//${myWeather.weather[0].icon}.png`;
     getUTCtimeStamp();
     console.log(currentWeather);
   } catch (e) {
@@ -34,7 +42,6 @@ function geoFindMe() {
     myLatitude = latitude;
     myLongitude = longitude;
     mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
-    mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
     getMyWeather(latitude, longitude);
   }
 
@@ -62,4 +69,33 @@ function getUTCtimeStamp(valNum) {
 
   return timestr;
 }
+async function getWeatherByCity() {
+  error_msg.innerHTML = "";
+
+  const api = `https://api.openweathermap.org/data/2.5/weather?q=${city_input_holder.value}&appid=${apiKey}`;
+  const reqData = await fetch(api);
+  const data = await reqData.json();
+  let cityWeather = "";
+  if (data.cod == 404) {
+    error_msg.innerHTML = "The city is not found ";
+  }
+  if (data.cod == 200) {
+    if (cities[data.name] === undefined) {
+      cities[data.name] = temperatureConverter(data.main.temp);
+      cityWeather = `${data.name}: ${temperatureConverter(data.main.temp)}`;
+      const cityElement = document.createElement("div");
+      cityElement.classList.add("search-weather-result");
+      cityElement.innerHTML = cityWeather + "\xB0C.";
+      search_weather.insertAdjacentElement("afterend", cityElement);
+    } else {
+      error_msg.innerHTML = "you already search for this city";
+    }
+  }
+  if (data.cod == 400) {
+    error_msg.innerHTML = "You didnt search for nothing ";
+  }
+  console.log(cities);
+}
+//EVENTS
+sumbit_btn.addEventListener("click", () => getWeatherByCity());
 window.onload = geoFindMe();
